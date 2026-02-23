e51942584c54ea65a35a4710ee89bfeb21b3950a
Praveen Verma <166142603+praveenstp09@users.noreply.github.com>
2026-02-23 14:06:16 +0530

    Update JwtMiddleware

--- Middlewares/JwtMiddleware.cs
+++ Middlewares/JwtMiddleware.cs
@@ -1,6 +1,31 @@
+using EmpLeave.Services;
 ﻿namespace Backend.Middlewares
 {
     public class JwtMiddleware
     {
+
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, TokenService tokenService)
+        {
+            var authHeader = context.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Bearer "))
+            {
+                var token = authHeader.Substring("Bearer ".Length).Trim();
+                var userId = tokenService.ValidateToken(token);
+                if (userId != null)
+                {
+                    context.Items["UserId"] = userId;
+                }
+            }
+
+            await _next(context);
+        }
     }
 }
