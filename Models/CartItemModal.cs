158abc2cd96498e119745d5aaddd2c7eea160df0
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2026-02-23 14:15:02 +0530

    Update CartItemModal.cs

--- Models/CartItemModal.cs
+++ Models/CartItemModal.cs
@@ -2,5 +2,12 @@
 {
     public class CartItemModal
     {
+            public int CartItemId { get; set; }
+    public int CartId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public CartModal Cart { get; set; } = null!;
+    public ProductModal Product { get; set; } = null!;
+}
     }
 }
