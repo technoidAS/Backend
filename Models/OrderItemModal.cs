a30d9e9c44d2490e68d17b8689ccde5154f7bbeb
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2026-02-23 14:14:10 +0530

    Add properties to OrderItemModal class

--- Models/OrderItemModal.cs
+++ Models/OrderItemModal.cs
@@ -2,5 +2,12 @@
 {
     public class OrderItemModal
     {
+         public int OrderItemId { get; set; }
+ public int OrderId { get; set; }
+ public int ProductId { get; set; }
+ public int Quantity { get; set; }
+ public decimal PriceOrder { get; set; }
+ public OrderModal Order { get; set; }
+ public ProductModal Product { get; set; }
     }
 }
