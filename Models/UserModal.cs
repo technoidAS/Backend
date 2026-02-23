b27d8f4910d2a8baf703f1776e623eff44b2a181
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2026-02-23 14:11:35 +0530

    Add properties to UserModal class

--- Models/UserModal.cs
+++ Models/UserModal.cs
@@ -2,5 +2,16 @@
 {
     public class UserModal
     {
+            public int UserId { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public string MobileNo { get; set; } = null!;
+    public bool IsAdmin { get; set; } = false;
+    public DateTime CreatedAt { get; set; }
+    public AddressModal? Address { get; set; }
+    public CartModal? Cart { get; set; }
+    public ICollection<OrderModal> Orders { get; set; } = new List<OrderModal>();
+}
     }
 }
