238b58ade10ffa89878184d91b6a1cbe594c2632
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2026-02-23 14:12:10 +0530

    Add properties to ProductModal class

--- Models/ProductModal.cs
+++ Models/ProductModal.cs
@@ -2,5 +2,14 @@
 {
     public class ProductModal
     {
+         public int ProductId { get; set; }
+ public string ProductName { get; set; } = null!;
+ public string? Description { get; set; }
+ public string Category { get; set; } = null!;
+ public decimal Price { get; set; }
+ public int Quantity { get; set; }
+ public string? ImageUrl { get; set; }
+ public bool IsAvailable { get; set; }
+ public DateTime CreatedAt { get; set; }
     }
 }
