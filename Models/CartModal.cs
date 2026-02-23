2c992507d5b233e2dc26148cd84220e8161cce1e
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2026-02-23 14:14:38 +0530

    Update CartModal.cs

--- Models/CartModal.cs
+++ Models/CartModal.cs
@@ -2,5 +2,11 @@
 {
     public class CartModal
     {
+                public int CartId { get; set; }
+        public int UserId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public UserModal User { get; set; } = null!;
+        public ICollection<CartItemModal> CartItems { get; set; } = new List<CartItemModal>();
     }
 }
